951df3b0f74791fa12499c2ed184d5616ed7c13f
wil92 <wil@aleph.engineering>
2019-02-26 22:18:13 -0500

    Update logs module for put more than one arg in the logs funcions

--- api/utils/logs.js
+++ api/utils/logs.js
@@ -1,15 +1,23 @@
+const reduce = require('lodash/reduce');
+
 const verbose = process.env['GL_VERBOSE'] || 'error';
 
 module.exports = exports = {
     logs: ['error', 'info', 'log'],
-    log: function (msg) {
-        this.weight('log') <= this.weight(verbose) && console.log(msg);
+    log: function () {
+        this.weight('log') <= this.weight(verbose) && console.log(this.reduce(arguments));
     },
-    info: function (msg) {
-        this.weight('info') <= this.weight(verbose) && console.info(msg);
+    info: function () {
+        this.weight('info') <= this.weight(verbose) && console.info(this.reduce(arguments));
     },
-    error: function (msg) {
-        this.weight('error') <= this.weight(verbose) && console.error(msg);
+    error: function () {
+        this.weight('error') <= this.weight(verbose) && console.error(this.reduce(arguments));
+        process.exit(1);
+    },
+    reduce: function (args) {
+        return reduce(args, function (arg, msg) {
+            return arg + (arg === '' ? '' : ' ') + msg;
+        }, '');
     },
     weight: function (log) {
         return this.logs.indexOf(log) === -1 ? this.logs.length : this.logs.indexOf(log);
