335df1548aa49daab2a60de4dfa84c02feac4e5f
wil92 <wil@aleph.engineering>
2019-02-26 22:36:12 -0500

    Add token option for authorization request

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const program = require("commander");
 
 const issues = require("./api/issues");
+const publishEnv = require("./api/utils/publish-env");
 const package = require("./package.json");
 
 
 program
     .version(package.version, '-v, --version')
-    .option('-i, --issues <action>', 'issues', issues, 'list')
+    .option('-t, --token <token>', 'user personal token', function (arg) {
+        publishEnv('GL_TOKEN', arg);
+    })
+    .option('-i, --issues <action>', 'issues', issues)
     .parse(process.argv);
