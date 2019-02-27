c26e7f216bec0aa2ebfee75a5facc2565aa83778
wil92 <wil@aleph.engineering>
2019-02-27 12:40:52 -0500

    Create mergeQueryParams method tests

--- api/utils/query-params-utils.test.js
+++ api/utils/query-params-utils.test.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const argsToQueries = require('./query-params-utils').argsToQueries;
 const mergeQueryParams = require('./query-params-utils').mergeQueryParams;
 
-describe('logs', function () {
+describe('query-params-utils', function () {
     describe('argsToQueries method', function () {
         it('should transform the example in an object', function () {
             expect(argsToQueries(['name=wil92', 'nickname=wil']))
@@ -16,6 +16,14 @@ describe('logs', function () {
     });
 
     describe('mergeQueryParams method', function () {
+        it('should generate the query params string for the url', function () {
+            expect(mergeQueryParams({name: 'wil92', nickname: 'wil'}))
+                .to.equal('name=wil92&nickname=wil');
+        });
 
+        it('should return an empty string', function () {
+            expect(mergeQueryParams({}))
+                .to.equal('');
+        });
     });
 });
