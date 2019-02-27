8681928b554a86b4c0e005cbb09a90d68d8554c7
wil92 <wil@aleph.engineering>
2019-02-26 23:11:46 -0500

    Fix issue with read first the env and then execure the actions

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const issues = require("./api/issues");
 const publishEnv = require("./api/utils/publish-env");
 const package = require("./package.json");
 
+loadProgram(true);
+loadProgram(false);
 
-program
-    .version(package.version, '-v, --version')
-    .option('-t, --token <token>', 'user personal token', function (arg) {
-        publishEnv('GL_TOKEN', arg);
-    })
-    .option('-i, --issues <action>', 'issues', issues)
-    .parse(process.argv);
+function loadProgram(readEnv) {
+    program
+        .version(package.version, '-v, --version')
+        .option('-t, --token <token>', 'user personal token', readEnv && function (arg) {
+            publishEnv('GL_TOKEN', arg);
+        })
+        .option('-i, --issues <action>', 'issues', !readEnv && issues)
+        .parse(process.argv);
+}
