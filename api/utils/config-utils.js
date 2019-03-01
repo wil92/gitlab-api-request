4ed109d372209319c4b9d5421f5b1598561d3ee9
wil92 <wil@aleph.engineering>
2019-02-28 23:11:50 -0500

    Update readConfig to merge with environments configurations

--- api/utils/config-utils.js
+++ api/utils/config-utils.js
@@ -9,8 +9,14 @@ const merge = require("lodash/merge");
 const homedir = os.homedir();
 const configFilePath = path.resolve(homedir, ".grconfig");
 
+module.exports.ENVS = {
+    "GR_TOKEN": "token",
+    "GR_VERBOSE": "verbose",
+    "GR_API_VERSION": "api_version",
+    "GR_URL": "url"
+};
+
 module.exports.readConfig = function (mergeWithEnv) {
-    mergeWithEnv = true;
     const configString = fs.existsSync(configFilePath) ? fs.readFileSync(configFilePath, "utf8") : null;
     let configData = configString ? TOML.parse(configString) : {};
     mergeWithEnv && (configData = merge(configData, envConfigs()));
@@ -22,9 +28,11 @@ module.exports.writeConfig = function (config) {
 };
 
 function envConfigs() {
+    const configs = {};
     forEach(process.env, function (value, key) {
         if (key.startsWith("GR_")) {
-            console.log(key, value);
+            configs[module.exports.ENVS[key]] = value;
         }
     });
+    return configs;
 }
